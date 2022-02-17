9d339d70645d4dde70442dbc5b52f46027163132
zerobeloved <799818616@qq.com>
2022-02-17 13:54:54 +0800

    finished crud

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
