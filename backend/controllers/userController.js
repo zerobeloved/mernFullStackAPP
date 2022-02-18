08e805083486104e44b41f265e8294080a0710c4
zerobeloved <799818616@qq.com>
2022-02-18 10:02:00 +0800

    add asyncHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,34 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userMoudle')
+
 // @desc      Register new user
 // @route     POST /api/users
 // @access    Public
-const registerUser =  (req, res) => {
+const registerUser =  asyncHandler(async (req, res) => {
+    const  {name, email, password } =req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please cheack add all fields')
+    }
     res.json({message: 'Register User'})
-}
+})
 
 // @desc      Authenticate a user
 // @route     POST /api/users/login
 // @access    Public
-const loginUser =  (req, res) => {
+const loginUser =  asyncHandler(async (req, res) => {
     res.json({message: 'login User'})
-}
+})
 
 // @desc      Get user data
 // @route     GET /api/users
 // @access    Public
-const getMe =  (req, res) => {
+const getMe =  asyncHandler(async (req, res) => {
     res.json({message: 'User data display'})
-}
+})
 
 
 
