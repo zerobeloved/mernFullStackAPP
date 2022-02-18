e25ec8e3227fc416862e9b2d30c94e3a338138d9
zerobeloved <799818616@qq.com>
2022-02-18 11:00:55 +0800

    add loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,14 +13,54 @@ const registerUser =  asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please cheack add all fields')
     }
-    res.json({message: 'Register User'})
+    //check if user exist
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc      Authenticate a user
 // @route     POST /api/users/login
 // @access    Public
 const loginUser =  asyncHandler(async (req, res) => {
-    res.json({message: 'login User'})
+    const {email, password} = req.body
+    
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+
 })
 
 // @desc      Get user data
