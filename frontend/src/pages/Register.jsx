bd66f1b797fe01ee3f63906cda53615d83f3a8b5
zerobeloved <799818616@qq.com>
2022-02-19 23:43:18 +0800

    add Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,58 @@
+import {useEffect, useState } from 'react'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
+
+    const {name, email, password, password2} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>Register</div>
+    <>
+        <section className="heading">
+            <h1>
+                <FaUser>Register</FaUser>
+            </h1>
+            <p>Please create an acount</p>
+        </section>
+
+        <section className="form">
+            <form onSubmit={onSubmit}></form>
+            <form>
+                <div className="form-group">
+                <input type="text" className="form-control" id="name" name="name" value={name} placeholder='Enter your name' onChange={onChange}/>                    
+                </div>
+                <div className="form-group">
+                <input type="text" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange}/>                    
+                </div>
+                <div className="form-group">
+                <input type="text" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange}/>                    
+                </div>
+                <div className="form-group">
+                <input type="text" className="form-control" id="password2" name="password2" value={password2} placeholder='Confirm your password' onChange={onChange}/>                    
+                </div>
+                <div className="form-group">
+                    <button type="submit" className='btn btn-block'>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </section>
+    </>
   )
 }
 
