9f6b27a93e2ea0c43a9f5ced8bc6e5c39b8baa07
zerobeloved <799818616@qq.com>
2022-02-24 17:29:23 +0800

    add goal

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,13 +69,8 @@ const loginUser =  asyncHandler(async (req, res) => {
 // @route     GET /api/users
 // @access    Private
 const getMe =  asyncHandler(async (req, res) => {
-    const {_id, name, email} = await User.findById(req.user.id) 
         
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-        })
+    res.status(200).json(req.user)
     }
 )
 
