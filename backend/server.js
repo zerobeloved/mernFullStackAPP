c8aa53090eee77e0e3fab8e8b865eb5b4a46f796
zerobeloved <799818616@qq.com>
2022-02-25 14:47:58 +0800

    deploy5

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-const path = require('path')
 const express = require('express')
-const dotenv = require('dotenv').config()
 const colors = require('colors')
-const {errorHandler} = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
+connectDB()
+
 const app = express()
 
-connectDB()
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//Serve frontend
+// Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-  
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-      )
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
-  } else {
-    app.get('/', (req, res) => res.send('Please set to production'))
-  }
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
-
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
